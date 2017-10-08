0da8320d336a6a9fc914c1f5be7431459a118a5e
edenhoferMarco <edenhofermarco@t-online.de>
2017-10-08 18:20:13 +0200

    added README.md to kurisu

--- kurisu/kurisu.py
+++ kurisu/kurisu.py
@@ -7,7 +7,10 @@ help_text = 'To use ' + bot_name + ', type - followed by a command.\n'  \
             'Available commands are:\n'                                 \
             '\t- test: no clue\n'                                       \
             '\t- sleep: sleeps for 5 seconds\n'                         \
-            '\t- help: prints out this help\n'
+            '\t- help: prints out this help\n'                          \
+            '\nor type @kurisu followed by a message.\n'                  \
+            'Available messages are:\n'                                \
+            '\t- smile\n'                                                  \
 
 @client.event
 async def on_ready():
@@ -27,10 +30,22 @@ async def on_message(message):
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
     elif message.content.startswith('-sleep'):
+        await client.send_message(message.channel, 'Zzzz...')
         await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+        await client.send_message(message.channel, 'Huh?! I... I did not sleep because you wanted me to do so! '
+                                                   'In fact I was just tired. Yeah tired, '
+                                                   'because you bore ma that much! Baka...')
     elif message.content.startswith('-help'):
         await client.send_message(message.channel, help_text)
+
+    elif message.content.startswith('@Kurisu') or message.content.startswith('@kurisu'):
+        index = message.content.find('@')
+        command = message.content[index + 7:].strip()
+
+        if command == 'smile':
+            await client.send_message(message.channel, '\*smiles\*')
+        else:
+            await client.send_message(message.channel, '\*sight\* what?')
 
 # replace with the token for your discord app/ dc bot
 client.run('token')
