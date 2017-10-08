f5d576ba80c4097a4d0032705ec677312fb6c50a
ThomasHildebrand <thomas1.hildebrand@st.oth-regensburg.de>
2017-10-08 16:24:49 +0200

    setup_2: Added new app, kurisu bot

---
+++ kurisu/kurisu.py
@@ -0,0 +1,36 @@
+import discord
+import asyncio
+
+client = discord.Client()
+bot_name = '•kurisu'
+help_text = 'To use ' + bot_name + ', type - followed by a command.\n'  \
+            'Available commands are:\n'                                 \
+            '\t- test: no clue\n'                                       \
+            '\t- sleep: sleeps for 5 seconds\n'                         \
+            '\t- help: prints out this help\n'
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('-test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('-sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+    elif message.content.startswith('-help'):
+        await client.send_message(message.channel, help_text)
+
+# replace with the token for your discord app/ dc bot
+client.run('token')
