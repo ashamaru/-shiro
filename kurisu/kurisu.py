8e609010924401e65a0c27656f50448dd473a1b5
ThomasHildebrand <thomas1.hildebrand@st.oth-regensburg.de>
2017-10-08 19:22:24 +0200

    issue#1: Added xml db for kurisu.

--- kurisu/kurisu.py
+++ kurisu/kurisu.py
@@ -1,13 +1,55 @@
 import discord
 import asyncio
+from xml.etree import ElementTree
+from random import randint
 
 client = discord.Client()
-bot_name = '•kurisu'
+bot_name = '• kurisu •'
 help_text = 'To use ' + bot_name + ', type - followed by a command.\n'  \
             'Available commands are:\n'                                 \
             '\t- test: no clue\n'                                       \
             '\t- sleep: sleeps for 5 seconds\n'                         \
             '\t- help: prints out this help\n'
+
+dir_root = '/path/to/root'
+path_to_db = dir_root + '/kurisu/db.xml'
+
+
+# db.xml database wrapper
+
+class KurisuDatabaseWrapper:
+    path_to_xml_db = None
+
+    def __init__(self, path_to_xml_db):
+        if path_to_xml_db is None:
+            raise Exception
+        # *
+        # todo validate path
+        # *
+        self.path_to_xml_db = path_to_xml_db
+
+    def get_random_reply(self, mood):
+        tree = ElementTree.parse(self.path_to_xml_db)
+        root = tree.getroot()
+        for categorie in list(root):
+            if mood == categorie.tag:
+                noe = int(categorie.get('count'))
+                specific = randint(0, noe-1)
+                return list(categorie)[specific].text
+        return None
+
+db_wrapper = KurisuDatabaseWrapper(path_to_db)
+
+
+# Future message pipeline
+
+def eval_mood(message):
+    if randint(0 ,1) == 0:
+        return 'angry'
+    return 'bored'
+
+
+# common discord api
 
 @client.event
 async def on_ready():
@@ -31,6 +73,11 @@ async def on_message(message):
         await client.send_message(message.channel, 'Done sleeping')
     elif message.content.startswith('-help'):
         await client.send_message(message.channel, help_text)
+    # reacts to kuriso in msg
+    elif 'kurisu' in message.content:
+        mood = eval_mood(message.content)
+        reply = db_wrapper.get_random_reply(mood)
+        await client.send_message(message.channel, reply)
 
 # replace with the token for your discord app/ dc bot
 client.run('token')
